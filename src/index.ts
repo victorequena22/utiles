9d595d4e4413d974b9d1a30b500536d563c5de16
victorequena22 <victorequena22@gmail.com>
2023-01-18 22:50:47 -0400

    data

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { zfill, numberFormat, timeFormat, formatoFecha, formatoHora, solicitud, fechaToNumber } from './Formatos';
 import { ToRGB, getMes } from './Converciones';
 import { portapapeles } from './Acciones';
-import { filtro, busqueda, indexOfCallback } from './Filtros';
+import { filtro, busqueda, indexOfCallback, ordenar } from './Filtros';
 import { semaforo } from './Semaforo';
 export {
     zfill, numberFormat, timeFormat, formatoFecha, formatoHora, solicitud, fechaToNumber,
-    ToRGB, getMes, filtro, busqueda, indexOfCallback, portapapeles, semaforo
+    ToRGB, getMes, filtro, busqueda, indexOfCallback, portapapeles, semaforo, ordenar
 }
